b3941750a5e8d2a4fcb8ea12c73320001f35c3f4
JoseSaul <40270646+JoseSaul@users.noreply.github.com>
2020-10-30 15:09:16 +0000

    Initial commit

---
+++ app/src/main/java/View/NoteActivity.kt
@@ -0,0 +1,16 @@
+package View
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.note.R
+
+class NoteActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_note)
+
+    }
+
+}
