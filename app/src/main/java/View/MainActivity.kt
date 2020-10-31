0457e2417ad08662e40b61fce0572497a90af1ef
JoseSaul <40270646+JoseSaul@users.noreply.github.com>
2020-10-31 18:17:34 +0000

    New Activity
    Note's list and Delete button

--- app/src/main/java/View/MainActivity.kt
+++ app/src/main/java/View/MainActivity.kt
@@ -1,6 +1,10 @@
 package View
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.note.R
 
@@ -10,6 +14,15 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val boton : Button = findViewById(R.id.EnterListButton)
+    }
+
+    fun PruebaEntrar(view: View){
+        val toast = Toast.makeText(applicationContext, "Hello", Toast.LENGTH_LONG)
+        toast.show()
+
+        val intent = Intent(this,NoteActivity::class.java)
+        startActivity(intent)
     }
 
 }
